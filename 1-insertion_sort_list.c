3c1310d05d29fe5a3e45309cbe4869d2c79705f5
tarik khadri <tarikkhadri11@gmail.com>
2024-01-20 21:31:24 +0100

    tasl 2

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ * in ascending order using the Insertion Sort algorithm.
+ * @list: Pointer to the head of the doubly linked list
+ */
+void insertion_sort_list(listint_t **list)
+{
+listint_t *current = (*list)->next;
+listint_t *temp, *predecessor;
+if (list == NULL || *list == NULL || (*list)->next == NULL)
+{
+return;
+}
+while (current != NULL)
+{
+temp = current;
+predecessor = temp->prev;
+while (predecessor != NULL && temp->n < predecessor->n)
+{
+if (temp->next != NULL)
+temp->next->prev = predecessor;
+if (predecessor->prev != NULL)
+{
+predecessor->prev->next = temp;
+}
+else
+{
+*list = temp;
+}
+temp->prev = predecessor->prev;
+predecessor->next = temp->next;
+temp->next = predecessor;
+predecessor->prev = temp;
+temp = predecessor->prev;
+predecessor = temp->prev;
+print_list(*list);
+}
+current = current->next;
+}
+}
--- sort.h
+++ sort.h
@@ -18,5 +18,7 @@ typedef struct listint_s
 } listint_t;
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
-
+void print_list(const listint_t *list);
+void insertion_sort_list(listint_t **list);
+void swap(listint_t **list, listint_t *a, listint_t *b);
 #endif
