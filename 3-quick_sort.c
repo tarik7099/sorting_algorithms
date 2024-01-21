de4112883c620b3a13517001fe11f01f45c562b3
tarik khadri <tarikkhadri11@gmail.com>
2024-01-21 03:56:46 +0100

    fix task 222

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,87 +1,88 @@
 #include "sort.h"
 
 /**
-* swap - Swaps two integers.
-* @item1: Pointer to the first integer.
-* @item2: Pointer to the second integer.
-*/
-void swap(int *item1, int *item2)
+ * swap - Swaps two integers in an array.
+ * @array: Pointer to the array.
+ * @idx1: Index of the first element.
+ * @idx2: Index of the second element.
+ */
+void swap(int *array, size_t idx1, size_t idx2)
 {
-int tmp;
+    int tmp;
 
-tmp = *item1;
-*item1 = *item2;
-*item2 = tmp;
+    tmp = array[idx1];
+    array[idx1] = array[idx2];
+    array[idx2] = tmp;
 }
 
 /**
-* Lomuto - Partition function for Quick Sort algorithm (Lomuto scheme).
-* @array: Pointer to the array to be sorted.
-* @lr: Lower bound of the array or subarray.
-* @ur: Upper bound of the array or subarray.
-* @size: size
-* Return: The final position of the pivot element.
-*/
-ssize_t Lomuto(int *array, ssize_t lr, ssize_t ur, size_t size)
+ * Lomuto - Partition function for Quick Sort algorithm (Lomuto scheme).
+ * @array: Pointer to the array to be sorted.
+ * @lr: Lower bound of the array or subarray.
+ * @ur: Upper bound of the array or subarray.
+ * @size: Size of the array.
+ *
+ * Return: The final position of the pivot element.
+ */
+size_t Lomuto(int *array, size_t lr, size_t ur, size_t size)
 {
-int pivot;
-ssize_t start, end;
+    int pivot;
+    size_t start, end;
 
-pivot = array[ur];
+    pivot = array[ur];
+    start = lr;
 
-start = lr;
-end = ur - 1;
+    for (end = lr; end < ur; end++)
+    {
+        if (array[end] < pivot)
+        {
+            if (start != end)
+            {
+                swap(array, start, end);
+                print_array(array, size);
+            }
+            start++;
+        }
+    }
 
-while (start <= end)
-{
-while (start <= end && array[start] < pivot)
-start++;
-
-while (start <= end && array[end] > pivot)
-end--;
-
-if (start < end)
-{
-swap(&array[start], &array[end]);
-print_array(array, size);
-start++;
-end--;
-}
-}
-
-swap(&array[start], &array[ur]);
-print_array(array, size);
-
-return (start);
+    swap(array, start, ur);
+    print_array(array, size);
+    return start;
 }
 
 /**
-* qs - Recursive function to perform Quick Sort.
-* @array: Pointer to the array to be sorted.
-* @lr: Lower bound of the array or subarray.
-* @up: Upper bound of the array or subarray.
-* @size: size
-*/
-void qs(int *array, ssize_t lr, ssize_t up, size_t size)
+ * qs - Recursive function to perform Quick Sort.
+ * @array: Pointer to the array to be sorted.
+ * @lr: Lower bound of the array or subarray.
+ * @up: Upper bound of the array or subarray.
+ * @size: Size of the array.
+ */
+void qs(int *array, size_t lr, size_t up, size_t size)
 {
-ssize_t clocl;
+    size_t clocl;
 
-if (lr < up)
-{
-clocl = Lomuto(array, lr, up, size);
-qs(array, lr, clocl - 1, size);
-qs(array, clocl + 1, up, size);
-}
+    if (lr < up)
+    {
+        clocl = Lomuto(array, lr, up, size);
+        if (clocl != up)
+        {
+            qs(array, clocl, up, size);
+        }
+        if (clocl != lr)
+        {
+            qs(array, lr, clocl - 1, size);
+        }
+    }
 }
 
 /**
-* quick_sort - Sorts an array of integers using the Quick Sort algorithm.
-* @array: Pointer to the array to be sorted.
-* @size: Size of the array.
-*/
+ * quick_sort - Sorts an array of integers using the Quick Sort algorithm.
+ * @array: Pointer to the array to be sorted.
+ * @size: Size of the array.
+ */
 void quick_sort(int *array, size_t size)
 {
-if (!array || size < 2)
-return;
-qs(array, 0, size - 1, size);
+    if (!array || size < 2)
+        return;
+    qs(array, 0, size - 1, size);
 }
--- sort.h
+++ sort.h
@@ -3,18 +3,18 @@
 
 #include <stdio.h>
 /**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
+* struct listint_s - Doubly linked list node
+*
+* @n: Integer stored in the node
+* @prev: Pointer to the previous element of the list
+* @next: Pointer to the next element of the list
+*/
 
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
@@ -22,8 +22,8 @@ void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void swaping(listint_t **list, listint_t *current, listint_t *previous);
 void selection_sort(int *array, size_t size);
-ssize_t Lomuto(int *array, ssize_t lr, ssize_t ur, size_t size);
-void swap(int *item1, int *item2);
+size_t Lomuto(int *array, size_t lr, size_t ur, size_t size);
+void swap(int *array, size_t idx1, size_t idx2);
 void quick_sort(int *array, size_t size);
-void qs(int *array, ssize_t lr, ssize_t up, size_t size);
+void qs(int *array, size_t lr, size_t up, size_t size);
 #endif
