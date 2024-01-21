f2f217e95e12dc49095e02be9f09dd05371d2c1b
tarik khadri <tarikkhadri11@gmail.com>
2024-01-21 01:58:28 +0100

    fix task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ * selection_sort - Sorts a array
+ * @array: array will be sorted
+ * @size: size of array
+ */
+void selection_sort(int *array, size_t size)
+{
+size_t i, j, min, temp;
+if (array == NULL || size < 2)
+{
+return;
+}
+for (i = 0; i < size - 1; i++)
+{
+min = i;
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[min])
+{
+min = j;
+}
+}
+if (min != i)
+{
+temp = array[min];
+array[min] = array[i];
+array[i] = temp;
+print_array(array, size);
+}
+}
+}
--- sort.h
+++ sort.h
@@ -21,4 +21,5 @@ void bubble_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void swaping(listint_t **list, listint_t *current, listint_t *previous);
+ void selection_sort(int *array, size_t size);
 #endif
